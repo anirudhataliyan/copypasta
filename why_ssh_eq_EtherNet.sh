7cd25656476c85b2a599467000abc82f7c8cb01a
Anirudha Taliyan <akt746083@gmail.com>
2023-08-12 16:18:19 +0530

    Create why_ssh_eq_EtherNet.sh

---
+++ why_ssh_eq_EtherNet.sh
@@ -0,0 +1,11 @@
+[frosty@frosty Simple-CTF]$ nmap -p- 10.10.48.223
+Starting Nmap 7.94 ( https://nmap.org ) at 2023-08-12 16:08 IST
+Nmap scan report for 10.10.48.223 (10.10.48.223)
+Host is up (0.15s latency).
+Not shown: 65532 filtered tcp ports (no-response)
+PORT     STATE SERVICE
+21/tcp   open  ftp
+80/tcp   open  http
+2222/tcp open  EtherNetIP-1
+
+Nmap done: 1 IP address (1 host up) scanned in 484.27 seconds
