8a51508a14dcb8973437d6d6dbe918b50780ab0f
Anirudha Taliyan <38790786+anirudhataliyan@users.noreply.github.com>
2024-07-17 13:59:10 +0530

    Create primeNumber.java

---
+++ primeNumber.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    } // to check if number is prime or not
+    public static void printPrimes(int range) {
+        for (int i = 2; i <= range; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + "\n");
+            }
+        }
+    } // print all the crap
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if(isPrime(number) == true){
+            printPrimes(number);
+        } else {
+            System.out.println("Not a prime number");
+        }
+    }
+}
